9ec1f2470de58babd4de2d4f24162754f7682f84
kailunyao <yaokailun@cvte.com>
2023-02-05 00:34:06 +0800

    add: 支持列表数字编号元素的解析

--- packages/docx2html/src/elements/list.js
+++ packages/docx2html/src/elements/list.js
@@ -2,29 +2,46 @@ import Paragraph from './p'
 
 export default class List extends Paragraph{
 	convert(){
-		var elParent=this.parent.content, ul=elParent.lastElementChild;
+		const elParent = this.parent.content;
+    let listTag = elParent.lastElementChild;
+
 		var listStyle=this.wordModel.getNumberingStyle()
-		var numId=listStyle.id, level=this.wordModel.getLevel()
+    const parentStyle = listStyle.getParentStyle()
+		const numId=listStyle.id;
+    const level=this.wordModel.getLevel();
+
+    let isOrder = false;
+
+    if (parentStyle && parentStyle.levels) {
+      const levels = parentStyle.levels.get(parseInt(level));
+      if (level) {
+        const numFmt = levels.values.numFmt;
+        isOrder = numFmt !== 'bullet'
+      }
+    }
+
+    const listType = isOrder ? 'ol' : 'ul';
 		
 		var makeStructure=function(parent){
-			ul=this.doc.createElement('ul')
-			ul.id=listStyle.id
-			ul.setAttribute('level',level)
-			this.constructor.addClass(ul,listStyle.getParentStyle().id)
-			parent.appendChild(ul)
+			listTag = this.doc.createElement(listType)
+			listTag.id=listStyle.id
+			listTag.setAttribute('level',level)
+			this.constructor.addClass(listTag, listStyle.getParentStyle().id)
+			parent.appendChild(listTag)
 		}.bind(this)
 		
-		if(!ul || ul.localName!='ul' || ul.id!=numId){
+		if(!listTag || listTag.localName !== listType || listTag.id!=numId){
 			makeStructure(elParent)
-		}else if(ul.getAttribute('level')!=level){
-			var possibleParent=ul.querySelector('[level="'+level+'"]')
+		} else if(listTag.getAttribute('level')!=level){
+			var possibleParent = listTag.querySelector('[level="'+level+'"]')
 			if(!possibleParent){
-				makeStructure(ul.querySelector('[level="'+(parseInt(level)-1)+'"]') || ul)
-			}else 
-				ul=possibleParent
+				makeStructure(listTag.querySelector('[level="'+(parseInt(level)-1)+'"]') || listTag)
+			} else {
+        listTag = possibleParent;
+      }
 		}
 		var li=this.doc.createElement('li')
-		ul.appendChild(li)
+		listTag.appendChild(li)
 		li.appendChild(this.content=this.createElement())
 		var marker=this.doc.createElement('span')
 		this.constructor.addClass(marker, 'marker')
